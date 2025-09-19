93139cef8757dc4b99312a520bebcfeee89a9c01
lateef sanusi <lateefsanusifgc@gmail.com>
2025-09-19 08:02:30 +0100

    Add simple GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/possystem/',
+  base: '/retailpos/',  // Changed from '/possystem/' to '/retailpos/'
   server: {
     port: 3000,
   },
